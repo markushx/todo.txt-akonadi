1bd86020f5ff64a77c719375e32bf86dbde26cdf
Markus Becker <markushx@gmail.com>
2011-09-16 17:48:17 +0200

    Cleanup unused variables, old todos, etc.

--- todo_txt_akonadiresource.cpp
+++ todo_txt_akonadiresource.cpp
@@ -19,7 +19,6 @@
 
 using namespace Akonadi;
 
-typedef boost::shared_ptr<KCalCore::Incidence> IncidencePtr;
 typedef boost::shared_ptr<KCalCore::Todo> TodoPtr;
 
 todo_txt_akonadiResource::todo_txt_akonadiResource( const QString &id )
@@ -39,10 +38,6 @@ todo_txt_akonadiResource::~todo_txt_akonadiResource()
 
 void todo_txt_akonadiResource::retrieveCollections()
 {
-  // TODO: this method is called when Akonadi wants to have all the
-  // collections your resource provides.
-  // Be sure to set the remote ID and the content MIME types
-
   kWarning() << "retrieve collections";
 
   Collection c;
@@ -80,15 +75,7 @@ void todo_txt_akonadiResource::retrieveItems( const Akonadi::Collection &collect
 
   kWarning() << "retrieve items";
 
-  // TODO: this method is called when Akonadi wants to know about all the
-  // items in the given collection. You can but don't have to provide all the
-  // data for each item, remote ID and MIME type are enough at this stage.
-  // Depending on how your resource accesses the data, there are several
-  // different ways to tell Akonadi when you are done.
-
-  // our collections have the mapped path as their remote identifier
   const QString todoFileName = collection.remoteId();
-
   QFile todoFile(todoFileName);
   if (!todoFile.open(QIODevice::ReadOnly | QIODevice::Text))
     return;
@@ -128,10 +115,6 @@ bool todo_txt_akonadiResource::retrieveItem( const Akonadi::Item &item,
 {
   Q_UNUSED( parts );
 
-  // TODO: this method is called when Akonadi wants more data for a
-  // given item.  You can only provide the parts that have been
-  // requested but you are allowed to provide all in one go
-
   // All data was read in in retrieveItems
 
   itemRetrieved( item );
@@ -149,17 +132,6 @@ void todo_txt_akonadiResource::configure( WId windowId )
 {
   Q_UNUSED( windowId );
 
-  // TODO: this method is usually called when a new resource is being
-  // added to the Akonadi setup. You can do any kind of user interaction here,
-  // e.g. showing dialogs.
-  // The given window ID is usually useful to get the correct
-  // "on top of parent" behavior if the running window manager applies any kind
-  // of focus stealing prevention technique
-  //
-  // If the configuration dialog has been accepted by the user by clicking Ok,
-  // the signal configurationDialogAccepted() has to be emitted, otherwise, if
-  // the user canceled the dialog, configurationDialogRejected() has to be emitted.
-
   const QString oldTodoTxtFile = Settings::self()->todoTxtFile();
   KUrl url;
   if ( !oldTodoTxtFile.isEmpty() )
@@ -170,7 +142,8 @@ void todo_txt_akonadiResource::configure( WId windowId )
   const QString title = i18nc( "@title:window", "Select TODO.txt file." );
   const QString filter = QString("");
 
-  const QString newTodoTxtFile = KFileDialog::getOpenFileName( url, filter, 0, title );
+  const QString newTodoTxtFile = KFileDialog::getOpenFileName( url, filter,
+							       0, title );
 
   if ( newTodoTxtFile.isEmpty() )
     return;
@@ -195,20 +168,11 @@ void todo_txt_akonadiResource::itemAdded( const Akonadi::Item &item,
 	     << ", remoteId=" << item.remoteId()
 	     << ", mimeType=" << item.mimeType();
 
-  // TODO: this method is called when somebody else, e.g. a client application,
-  // has created an item in a collection managed by your resource.
-
-  // NOTE: There is an equivalent method for collections, but it isn't part
-  // of this template code to keep it simple
-
-  KCalCore::Todo::Ptr todo;
-  IncidencePtr ptrEvent;
-
   kWarning() << "hasPayload()" << item.hasPayload();
 
   if (item.hasPayload<KCalCore::Todo::Ptr>()) {
-    kWarning() << "todo::Ptr ";// << todo->summary();
-    todo = item.payload<KCalCore::Todo::Ptr>();
+    kWarning() << "todo::Ptr ";
+    KCalCore::Todo::Ptr todo = item.payload<KCalCore::Todo::Ptr>();
 
     if (todo) {
       kWarning() << "summary: " << todo->summary();
@@ -217,7 +181,11 @@ void todo_txt_akonadiResource::itemAdded( const Akonadi::Item &item,
       QFile todoFile(todoFileName);
 
       if (!todoFile.open(QIODevice::Append | QIODevice::Text)) {
-	// TODO: Indicate fail.
+	kError() << "Could not open TODO file.";
+	const QString message = i18nc("@info:status",
+				      "Could not open TODO file.");
+	emit error(message);
+	emit status(Broken, message);
 	return;
       }
 
@@ -228,7 +196,8 @@ void todo_txt_akonadiResource::itemAdded( const Akonadi::Item &item,
       QCryptographicHash hash( QCryptographicHash::Sha1 );
       hash.addData( todo->summary().toUtf8() );
 
-      QString remoteId(todoFileName + QLatin1Char( '/' ) + QString(hash.result().toHex()));
+      QString remoteId(todoFileName + QLatin1Char( '/' )
+		       + QString(hash.result().toHex()));
 
       Item newItem( item );
       newItem.setRemoteId( remoteId );
@@ -247,7 +216,8 @@ void todo_txt_akonadiResource::itemAdded( const Akonadi::Item &item,
   kWarning() << "itemAdded~";
 }
 
-void todo_txt_akonadiResource::itemChanged( const Akonadi::Item &item, const QSet<QByteArray> &parts )
+void todo_txt_akonadiResource::itemChanged( const Akonadi::Item &item,
+					    const QSet<QByteArray> &parts )
 {
   Q_UNUSED( parts );
 
@@ -330,7 +300,8 @@ void todo_txt_akonadiResource::itemChanged( const Akonadi::Item &item, const QSe
       kWarning() << "Removing file " << todoFileName;
       todoFile.remove();
 
-      kWarning() << "Renaming file " << todoFileName << ".tmp to "<< todoFileName;
+      kWarning() << "Renaming file " << todoFileName
+		 << ".tmp to "<< todoFileName;
       todoTmpFile.rename(todoFileName);
 
       kWarning() << "Adding watch on file " << todoFileName;
@@ -339,7 +310,8 @@ void todo_txt_akonadiResource::itemChanged( const Akonadi::Item &item, const QSe
       QCryptographicHash hash( QCryptographicHash::Sha1 );
       hash.addData( todo->summary().toUtf8() );
 
-      QString remoteId(todoFileName + QLatin1Char( '/' ) + QString(hash.result().toHex()));
+      QString remoteId(todoFileName + QLatin1Char( '/' )
+		       + QString(hash.result().toHex()));
 
       Item newItem( item );
       newItem.setRemoteId( remoteId );
